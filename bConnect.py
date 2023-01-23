eba4763461bf8abcf2bbd20c87b5ec755c375e4d
Geoff Rosenberg <geoff.rosenberg@gmail.com>
2023-01-22 21:48:13 -0800

    Add docstrings, default to latin1 client encoding.

--- bConnect.py
+++ bConnect.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
+"""Take a job number and list size of all files backed up in that job."""
 import psycopg2
 import getpass
 import argparse
 
-# Takes a job number (default most recent) and list all files backed up, along
-# with their sizes.
-
-# decode_stats taken from https://gist.github.com/Xiol/ee9d6e9d44494ea8df85
-B64_VALS = {
-    '+': 62, '/': 63, '1': 53, '0': 52, '3': 55, '2': 54, '5': 57,
-    '4': 56, '7': 59, '6': 58, '9': 61, '8': 60, 'A': 0, 'C': 2,
-    'B': 1, 'E': 4, 'D': 3, 'G': 6, 'F': 5, 'I': 8, 'H': 7, 'K': 10,
-    'J': 9, 'M': 12, 'L': 11, 'O': 14, 'N': 13, 'Q': 16, 'P': 15,
-    'S': 18, 'R': 17, 'U': 20, 'T': 19, 'W': 22, 'V': 21, 'Y': 24,
-    'X': 23, 'Z': 25, 'a': 26, 'c': 28, 'b': 27, 'e': 30, 'd': 29,
-    'g': 32, 'f': 31, 'i': 34, 'h': 33, 'k': 36, 'j': 35, 'm': 38,
-    'l': 37, 'o': 40, 'n': 39, 'q': 42, 'p': 41, 's': 44, 'r': 43,
-    'u': 46, 't': 45, 'w': 48, 'v': 47, 'y': 50, 'x': 49, 'z': 51
-}
-
 
 def decode_lstats(stats):
+    """Decode the lstats field."""
+    # taken from
+    # https://gist.github.com/Xiol/ee9d6e9d44494ea8df85
+    # (Xiol/bacula_b64decode.py)
     fields = """st_dev st_ino st_mode st_nlink st_uid st_gid
     st_rdev st_size st_blksize st_blocks st_atime st_mtime
     st_ctime LinkFI st_flags data""".split()
     out = {}
+
+    B64_VALS = {
+        '+': 62, '/': 63, '1': 53, '0': 52, '3': 55, '2': 54, '5': 57,
+        '4': 56, '7': 59, '6': 58, '9': 61, '8': 60, 'A': 0, 'C': 2,
+        'B': 1, 'E': 4, 'D': 3, 'G': 6, 'F': 5, 'I': 8, 'H': 7, 'K': 10,
+        'J': 9, 'M': 12, 'L': 11, 'O': 14, 'N': 13, 'Q': 16, 'P': 15,
+        'S': 18, 'R': 17, 'U': 20, 'T': 19, 'W': 22, 'V': 21, 'Y': 24,
+        'X': 23, 'Z': 25, 'a': 26, 'c': 28, 'b': 27, 'e': 30, 'd': 29,
+        'g': 32, 'f': 31, 'i': 34, 'h': 33, 'k': 36, 'j': 35, 'm': 38,
+        'l': 37, 'o': 40, 'n': 39, 'q': 42, 'p': 41, 's': 44, 'r': 43,
+        'u': 46, 't': 45, 'w': 48, 'v': 47, 'y': 50, 'x': 49, 'z': 51
+    }
 
     for i, element in enumerate(stats.split()):
         result = 0
@@ -38,6 +39,7 @@ def decode_lstats(stats):
 
 
 def Connect():
+    """Connect to the database and get job information."""
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--database', default='bacula',
                         help='Which database to connect to')
@@ -56,23 +58,25 @@ def Connect():
                         action='store_true')
 
     args = parser.parse_args()
-    PW = getpass.getpass()
+    PW = getpass.getpass('Password for user {0}: '.format(args.user))
 
     conn = psycopg2.connect(database=args.database,
                             user=args.user,
                             password=PW,
                             host=args.host,
                             port=args.port)
-    conn.set_client_encoding('UTF8')
+    # This was just a lucky guess about the client encoding
+    conn.set_client_encoding('LATIN1')
     cursor = conn.cursor()
 
     job = args.job
     if job == '0':
         try:
-            cursor.execute('SELECT clientid,jobid ' +
-                           'FROM job ' +
-                           'ORDER BY endtime DESC ' +
-                           'LIMIT 1')
+            cursor.execute('SELECT clientid,jobid '
+                           + 'FROM job '
+                           + 'WHERE jobstatus = \'T\''
+                           + 'ORDER BY endtime DESC '
+                           + 'LIMIT 1')
             rows = cursor.fetchall()
             job = str(rows[0][1])
             clientID = str(rows[0][0])
@@ -93,8 +97,8 @@ def Connect():
                        + 'JOIN path ON path.pathid = file.pathid '
                        + 'WHERE file.jobid = '
                        + job
-                       + ' AND filename.name != \'\''
-                       )
+                       + ' AND filename.name != \'\'')
+
     except psycopg2.OperationalError as e:
         print("Query failed with error " + e.pgerror)
 
@@ -114,6 +118,7 @@ def Connect():
             print("{0}{1}: {2}".format(record[0], record[1], out["st_size"]))
 
     if args.summarize:
+        print("\nStatistics for job {0}".format(job))
         cursor.execute('SELECT name FROM client WHERE clientid = '
                        + clientID)
         name = cursor.fetchall()
